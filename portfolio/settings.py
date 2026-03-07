7b9b1daece18321174db2c3fdb3f16b3798433c6
Sandwip Das <sandipdas050@gmail.com>
2026-03-07 18:17:50 +0600

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,7 +21,7 @@ load_dotenv(dotenv_path=BASE_DIR / '.env')
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('portfolio-2ydr.onrender.com', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # DATABASE CONFIGURATION
 # Render uses DATABASE_URL
