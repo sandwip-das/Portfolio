ec6f9f47624170003054fedce4f0d7c36c2cfec0
Sandwip Das <sandipdas050@gmail.com>
2026-03-08 01:19:56 +0600

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ if not SECRET_KEY:
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "portfolio-2ydr.onrender.com").split(",")
 
 # Database
 DATABASES = {
