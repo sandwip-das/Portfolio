35a133aa95d063f1d089ce15d501aab39439c206
Sandwip Das <sandipdas050@gmail.com>
2026-03-08 01:44:25 +0600

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -93,6 +93,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Allauth Middleware
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
