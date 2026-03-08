f976d9b54a4ad45b4c199ac6f9f9a5cd557d19ce
Sandwip Das <sandipdas050@gmail.com>
2026-03-09 01:02:34 +0600

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -170,7 +170,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-     "Allauth Middleware",
     "allauth.account.middleware.AccountMiddleware",
 ]
 
