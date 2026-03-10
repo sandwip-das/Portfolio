03e6bbf639285a6ee6be63fa4087a6c5fb0c6394
Sandwip Das <sandipdas050@gmail.com>
2026-03-10 18:23:46 +0600

    Updated: 10 Mar 2026, AT 18:23
    Make migrate

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -17,6 +17,8 @@ import dj_database_url
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
 
+# from dotenv import load_dotenv
+# load_dotenv()  # Load .env variables automatically
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,7 +52,7 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = not DEBUG
 CSRF_COOKIE_SECURE = not DEBUG
 
-# Database
+# Database for Render
 DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
@@ -218,16 +220,29 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv("DB_NAME"),
-#         'USER': os.getenv("DB_USER"),
-#         'PASSWORD': os.getenv("DB_PASSWORD"),
-#         'HOST': os.getenv("DB_HOST"),
-#         'PORT': os.getenv("DB_PORT"),
+# # For testing Detect if running on Render
+# RENDER = os.environ.get("RENDER") == "true"
+#
+# if RENDER:
+#     # Production DB (Render PostgreSQL)
+#     DATABASES = {
+#         "default": dj_database_url.config(
+#             default=os.environ.get("DATABASE_URL"),
+#             conn_max_age=600,
+#         )
 #     }
-# }
+# else:
+#     # Local DB
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql",
+#             "NAME": os.environ.get("DB_NAME", "myportfolio_db"),
+#             "USER": os.environ.get("DB_USER", "postgres"),
+#             "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+#             "HOST": os.environ.get("DB_HOST", "localhost"),
+#             "PORT": os.environ.get("DB_PORT", "5432"),
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
