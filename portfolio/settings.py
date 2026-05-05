2d96f2f69fb49818088a35c1758ca44d0db9290d
Sandwip Das <sandipdas050@gmail.com>
2026-05-05 19:18:48 +0600

    Updated: 05 May 26, At 19:18
    Fix images issue

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -256,9 +256,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # CKEditor + Cloudinary
 CKEDITOR_5_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
-# Default storage for media (uploads)
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME"),
     'API_KEY': os.environ.get("CLOUDINARY_API_KEY"),
@@ -284,8 +281,6 @@ STORAGES = {
 }
 
 cloudinary_name = os.environ.get("CLOUDINARY_CLOUD_NAME", "")
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
