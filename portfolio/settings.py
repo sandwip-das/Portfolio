07e4266c81fe4554eb25911be502578bd367844e
Sandwip Das <sandipdas050@gmail.com>
2026-05-05 17:27:15 +0600

    Updated: 05 May 26, At 17:26
    Not deployed for db config problem,

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -54,7 +54,11 @@ CSRF_COOKIE_SECURE = not DEBUG
 
 ## ===================== DATABASE =====================
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # ===================== CELERY CONFIG =====================
